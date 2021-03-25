caa04fd5ea18602cad6fec6dda68aad297f86d6a
Aaron <amauro@brocku.ca>
2021-03-25 10:16:06 -0400

    update paths

--- src/containers/NavExpand/NavExpand.js
+++ src/containers/NavExpand/NavExpand.js
@@ -11,11 +11,11 @@ class NavExpand extends Component {
     {
         return(
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/">N</Navbar.Brand>
+                <Navbar.Brand href="react_notes_pwa/">N</Navbar.Brand>
                 <Nav className="mr-auto">
-                <Nav.Link href="/">New</Nav.Link>
-                <Nav.Link href="/notes">Notes</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link href="react_notes_pwa/">New</Nav.Link>
+                <Nav.Link href="react_notes_pwa/notes">Notes</Nav.Link>
+                <Nav.Link href="react_notes_pwa/about">About</Nav.Link>
                 </Nav>
             </Navbar>
                 
